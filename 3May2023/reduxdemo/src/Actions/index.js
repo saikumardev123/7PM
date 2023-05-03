96065a39796e30738c6061799908b1b4b991edd4
sai <sai@digital-lync.com>
2023-05-03 20:30:50 +0530

    3may

---
+++ 3May2023/reduxdemo/src/Actions/index.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+export default function getProducts() {
+    var promise = axios.get("https://fakestoreapi.com/products");
+    return {
+        payload: promise,
+        type: "GET_PRODUCTS"
+    }
+}
+
+getProducts();
